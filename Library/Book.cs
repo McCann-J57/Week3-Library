77d8328bf9b48ceb7b1dde61aee0cfd3814ac8b0
McCann-J57 <McCann-J57@ulster.ac.uk>
2025-10-06 16:50:15 +0100

    Updated code with access modifiers.

--- Library/Book.cs
+++ Library/Book.cs
@@ -30,13 +30,5 @@ namespace Library
             Console.WriteLine();
         }
 
-        static void Main(string[] args)
-        {
-            //Create a new instance (object) of the Book class
-            //Note how the object name differs from the class name
-            Book book = new Book("C# for beginners", "Bill Gates", "1234567");
-
-            book.DisplayInfo();
-        }
-    }
+    }   
 }
