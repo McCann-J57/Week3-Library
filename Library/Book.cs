f8a6051482965285c8fe1af35e6f6f03d68ebd67
McCann-J57 <McCann-J57@ulster.ac.uk>
2025-10-06 17:06:36 +0100

    Updated code with variables and properties.

--- Library/Book.cs
+++ Library/Book.cs
@@ -6,29 +6,48 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    internal class Book
+    class Book
     {
-        string Title;
-        string Author;
-        string ISBN;
+        private string title; // variable
+        private string author;// variable
+        private string isbn; // variable
 
-        // Example of a constructor that allows us to 'construct' a new
-        // Book object
-        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        //Title property to allow access
+        //to the title private variable
+
+        public string Title
         {
-            Title = bookTitle;
-            Author = bookAuthor;
-            ISBN = bookISBN;
+            get { return title; } // get method
+            set { title = value; } // set method
+        }
+        public string Author
+        {
+            get { return Author; }
+            set { Author = value; }
         }
 
-        // Example of a method within the Book class
+        public string ISBN
+        {
+            get { return ISBN; }
+            set { ISBN = value; }
+        }
+
+        // Constructor to add a new book
+        public Book(string bookTitle, string bookAuthor, string bookISBN)
+        {
+            this.Title = bookTitle;
+            this.Author = bookAuthor;
+            this.ISBN = bookISBN; 
+        }
+
+        // Method to display information about A BOOK
         public void DisplayInfo()
         {
-            Console.WriteLine($"Book title: {Title}");
+            Console.WriteLine($"Book Title: {Title}");
             Console.WriteLine($"Book Author: {Author}");
             Console.WriteLine($"Book ISBN: {ISBN}");
             Console.WriteLine();
-        }
 
-    }   
+        }
+    }  
 }
